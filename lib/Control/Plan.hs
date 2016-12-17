8694406c3217726f403651009af079e9ccc169f9
daniel <daniel@bogusemailserver.com>
2016-12-17 12:10:05 +0100

    pruned some imports

--- lib/Control/Plan.hs
+++ lib/Control/Plan.hs
@@ -17,8 +17,8 @@ module Control.Plan (
                     -- * Analyzing plans
                     ,getSteps
                     ,Steps
-                    ,Mandatoriness(..)
                     ,mandatoriness
+                    ,Mandatoriness(..)
                     ,foldSteps
                     -- * Adapting plans
                     ,bimapSteps
@@ -38,28 +38,19 @@ module Control.Plan (
                     -- * The Lasagna typeclass
                     ,Lasagna(..)
                     -- * Re-exports
-                    ,Control.Comonad.extract
                     ,Data.Bifunctor.bimap
                     ,Data.Bifoldable.bifoldMap
                     ,Data.Bitraversable.bitraverse
+                    ,Control.Comonad.extract
                     ,Streaming.hoist
                     ,Streaming.Prelude.effects
                     ,mapTickM
                     ) where
 
-import Prelude hiding ((.),id)
-import Data.Tree
 import Data.Bifunctor
 import Data.Bifoldable
 import Data.Bitraversable
-import Data.Profunctor (Profunctor(..),Star(..))
-import Control.Category
-import Control.Applicative
 import Control.Comonad
-import Control.Monad
-import Control.Monad.IO.Class
-import Control.Monad.Trans.Class
-import Control.Arrow
 import Streaming
 import Streaming.Prelude
 
